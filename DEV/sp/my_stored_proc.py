19536c7a7421275a46592968260072aad9b8c12a
kumoora <132431831+kumoora@users.noreply.github.com>
2025-05-13 13:52:14 +0530

    Update my_stored_proc.py

--- DEV/sp/my_stored_proc.py
+++ DEV/sp/my_stored_proc.py
@@ -1,7 +1,7 @@
 def process_score(score: int) -> str:
     if score < 0 or score > 100:
         raise ValueError("Score must be between 0 and 100")
-    elif score >= 90:
+    elif score >= 89:
         return "Excellent"
     elif score >= 75:
         return "Good"
